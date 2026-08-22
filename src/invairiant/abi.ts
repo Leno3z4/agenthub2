3287b611be4616bc3651efa60ce7b2665a6fc156
leo <leno3z4rrrri@gmail.com>
2026-08-22 21:37:55 +0100

    Fix InvAIriant factory and execute ABIs

--- src/invairiant/abi.ts
+++ src/invairiant/abi.ts
@@ -1,10 +1,15 @@
 export const factoryAbi = [
-  { type: "function", name: "createAccount", stateMutability: "nonpayable", inputs: [], outputs: [{ name: "account", type: "address" }] },
+  { type: "function", name: "createAccount", stateMutability: "nonpayable", inputs: [{ name: "operator", type: "address" }], outputs: [{ name: "account", type: "address" }] },
   { type: "function", name: "getAccount", stateMutability: "view", inputs: [{ name: "owner", type: "address" }], outputs: [{ name: "account", type: "address" }] },
 ] as const;
 
 export const accountAbi = [
   { type: "function", name: "owner", stateMutability: "view", inputs: [], outputs: [{ type: "address" }] },
-  { type: "function", name: "operator", stateMutability: "view", inputs: [], outputs: [{ type: "address" }] },
-  { type: "function", name: "execute", stateMutability: "payable", inputs: [{ name: "target", type: "address" }, { name: "value", type: "uint256" }, { name: "data", type: "bytes" }], outputs: [{ type: "bytes" }] },
+  { type: "function", name: "execute", stateMutability: "payable", inputs: [{ name: "target", type: "address" }, { name: "data", type: "bytes" }], outputs: [{ type: "bytes" }] },
+  { type: "function", name: "setApproval", stateMutability: "nonpayable", inputs: [{ name: "token", type: "address" }, { name: "spender", type: "address" }, { name: "amount", type: "uint256" }] },
+  { type: "function", name: "setProtocolWhitelisted", stateMutability: "nonpayable", inputs: [{ name: "protocol", type: "address" }, { name: "whitelisted", type: "bool" }] },
+  { type: "function", name: "setProtocolSelectorsAllowed", stateMutability: "nonpayable", inputs: [{ name: "protocol", type: "address" }, { name: "selectors", type: "bytes4[]" }, { name: "allowed", type: "bool" }] },
+  { type: "function", name: "setTokenWhitelisted", stateMutability: "nonpayable", inputs: [{ name: "token", type: "address" }, { name: "whitelisted", type: "bool" }] },
+  { type: "function", name: "configureOperator", stateMutability: "nonpayable", inputs: [{ name: "operator", type: "address" }, { name: "skills", type: "uint256" }, { name: "maxLeverageHdths", type: "uint16" }] },
+  { type: "function", name: "setSpendingLimit", stateMutability: "nonpayable", inputs: [{ name: "operator", type: "address" }, { name: "token", type: "address" }, { name: "maxAmount", type: "uint128" }, { name: "periodSeconds", type: "uint32" }] },
 ] as const;
