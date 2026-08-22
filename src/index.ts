c7f88728af19a0835574c08da60b3e329aacb688
leo <leno3z4rrrri@gmail.com>
2026-08-22 21:20:44 +0100

    feat: verify Perpl context on startup

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,19 @@
 import "dotenv/config";
+import { getPerplContext } from "./perpl/api.js";
 import { verifyPerplDeployment } from "./perpl/client.js";
 
-const result = await verifyPerplDeployment();
-console.log(JSON.stringify(result, null, 2));
+const [deployment, context] = await Promise.all([
+  verifyPerplDeployment(),
+  getPerplContext(),
+]);
+
+console.log(JSON.stringify({
+  deployment,
+  markets: context.markets.map(({ id, symbol, config, state }) => ({
+    id,
+    symbol,
+    open: config.is_open,
+    bid: state.bid,
+    ask: state.ask,
+  })),
+}, null, 2));
