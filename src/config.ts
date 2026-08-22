db0b5125a7923d0c7d47b50f58be36f578afb2cb
leo <leno3z4rrrri@gmail.com>
2026-08-22 21:15:25 +0100

    fix Monad InvAIriant factory address

--- src/config.ts
+++ src/config.ts
@@ -8,7 +8,8 @@ export const monad = defineChain({
 });
 
 export const PERPL_EXCHANGE: Address = "0x34B6552d57a35a1D042CcAe1951BD1C370112a6F";
-export const PERPL_DELEGATED_ACCOUNT_FACTORY: Address = "0xc535276e3e446e4f28d95ed27ccd5c32e4c8907a";
+export const INVAIRIANT_FACTORY: Address = "0xb54B83513519Ec64e579F8F1CDdeaEF1CF4BB71b";
+export const INVAIRIANT_IMPLEMENTATION: Address = "0x0CBBaB6F3f5915EBe3054Af76ef7e5c638AADa2e";
 
 export const publicClient = createPublicClient({
   chain: monad,
