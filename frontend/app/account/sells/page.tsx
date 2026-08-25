0894b1111bca3b99e01c9091ef63a78d8300af84
leo <leno3z4rrrri@gmail.com>
2026-08-26 00:14:09 +0100

    Add account sells page

---
+++ frontend/app/account/sells/page.tsx
@@ -0,0 +1,10 @@
+import { AccountSectionPage } from "../../../components/AccountSectionPage";
+
+export default function SellsPage() {
+  return (
+    <AccountSectionPage
+      title="Sells"
+      description="Review sell-side executions made through this delegated account."
+    />
+  );
+}
