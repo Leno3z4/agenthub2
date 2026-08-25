8937fdae3ec8d2c9389398f9e50e2c315c34012e
leo <leno3z4rrrri@gmail.com>
2026-08-26 00:13:54 +0100

    Add account buys page

---
+++ frontend/app/account/buys/page.tsx
@@ -0,0 +1,10 @@
+import { AccountSectionPage } from "../../../components/AccountSectionPage";
+
+export default function BuysPage() {
+  return (
+    <AccountSectionPage
+      title="Buys"
+      description="Review buy-side executions made through this delegated account."
+    />
+  );
+}
